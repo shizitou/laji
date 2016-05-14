3e243b1ed2c2556ed4b2a71a1d1566c411a7b02e
shi zi tou <liuhaimingyy@163.com>
2016-05-14 19:46:19 +0800

    update util: link, gPHash

---
+++ release/brick/bk.template.js
@@ -0,0 +1,162 @@
+//模板引擎
+define('$Compenent',['$template'],function (require,exports,module) {
+    var $template = require('$template');
+    var compsList = {};
+    var rep_style = /((?:[^\{\}\s]+\s*)+)(\{.*?\})/g;
+    var rep_nodeName = /^\s*<(\w+)/;
+    var prefixId = 1;
+    var style = document.createElement('style');
+    var tableRow = document.createElement('tr');
+    var containers = {
+        'tr': document.createElement('tbody'),
+        'td': tableRow,
+        'th': tableRow,
+        '*': document.createElement('div')
+    };
+    document.head.appendChild(style);
+    function addStyle(css,prefixClass){
+        var lineCss,styleKey;
+        var result = "";
+        while(lineCss = rep_style.exec(css)){
+            styleKey = lineCss[1];
+            styleKey = styleKey.split(/,\s*/).map(function(cssStr){
+                return cssHandle(cssStr,'.'+prefixClass);
+            }).filter(function(cssStr){
+                return cssStr;
+            });
+            result += styleKey.join(',')+lineCss[2];
+        }
+        style.appendChild( document.createTextNode(result) );
+    }
+    function cssHandle(styleKey, prefixClass) {
+        var mainKey;
+        if (styleKey) {
+            styleKey = styleKey.split(/\s+/);
+            mainKey = styleKey.shift();
+            styleKey = styleKey.length ? ' ' + styleKey.join(' ') : '';
+            return mainKey + prefixClass + styleKey + ',' +
+                prefixClass + ' ' + mainKey + styleKey;
+        }
+        return '';
+    }
+    function addClassToRenView(origin,profixClass){
+        var nodeName,container;
+        nodeName = origin.match(rep_nodeName);
+        nodeName = nodeName && nodeName[1];
+        container = containers[nodeName] || containers['*'];
+        container.innerHTML = origin;
+        origin = container.children;
+        for(var i=origin.length-1;i>=0;i--){
+            origin[i].className = origin[i].className+' '+profixClass;
+        }
+        return container.innerHTML;
+    }
+    module.exports = function (compId,compObj) {
+        compObj.complie = $template(compObj.tpl);
+        compsList[compId] = {
+            resource: compObj,
+            compStyle: null
+        };
+        compObj = null;
+        return function(data) {
+            var compObj = compsList[compId];
+            var compResObj = compObj.resource;
+            var compStyle = compObj.compStyle;
+            //处理css到页面当中
+            if (compResObj.css && !compStyle) {
+                compObj.compStyle = compStyle = 'bkcomps-' + prefixId++;
+                addStyle(compResObj.css, compStyle);
+            }
+            return addClassToRenView(
+                compResObj.complie(data), compStyle
+            );
+        };
+    }
+});
+define('$template',[],function(require){
+	/****************
+    这是从underscore源码中提取出来的模板引擎,
+    源码中的调用方法是 _.template();
+    ****************/
+    return function(text, data) {
+        var compenent = {
+            require: function(compId,renderData){
+                return require(compId)(renderData || data);
+            }
+        };
+        //存放的是组合后的渲染函数
+        var render,
+            //匹配模板标签
+            settings = {
+                evaluate    : /<%([\s\S]+?)%>/g,
+                interpolate : /<%=([\s\S]+?)%>/g,
+                escape      : /<%-([\s\S]+?)%>/g
+            },
+            //转意成实体,用于过滤html标签
+            entityMap = {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#x27;'
+            },
+            //转意实体的调用方法,当遇到 <%- %> 时,调用
+            _escape = function (string) {
+                if (string == null) 
+                    return '';
+                return ('' + string).replace( /[&<>"']/g , function(match){
+                    return entityMap[ match ];
+                });
+            },
+            //转意空格之类的字符
+            escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g,
+            escapes = {
+                "'": "'",
+                '\\': '\\',
+                '\r': 'r',
+                '\n': 'n',
+                '\t': 't',
+                '\u2028': 'u2028',
+                '\u2029': 'u2029'
+            },
+            //解析模板标签的正则队列
+            matcher = new RegExp([
+                ( settings.escape ).source,
+                ( settings.interpolate ).source,
+                ( settings.evaluate ).source
+            ].join('|') + '|$', 'g'),
+            //最终拼接字符串时,跟中的下标
+            index = 0,
+            //最终执行的匹配后的字符串
+            source = "__p+='";
+        //拼接各种形式的语句 => source
+        text.replace(matcher, function(match, escape, interpolate, evaluate, offset){
+            source += text.slice(index, offset).replace(escaper, function(match){ 
+                return '\\' + escapes[match];
+            });
+            if(escape)
+                source += "'+\n((__t=(" + escape + "))==null?'':_escape(__t))+\n'";
+            if(interpolate)
+                source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+            if(evaluate)
+                source += "';\n" + evaluate + "\n__p+='";
+            index = offset + match.length;
+            return match;
+        });
+        source += "';\n";
+        source = 'with(obj||{}){\n' + source + '}\n';
+        source = "var __t,__p='',__j=Array.prototype.join," +
+            "print=function(){__p+=__j.call(arguments,'');};\n" +
+            source + "return __p;\n";
+        //这里可能会抛异常，请务必保持资源的合法性
+        render = new Function('obj', '_escape' , source);
+        //当没有传入data时 返回的方法: 颗粒模式
+        if(data===undefined)
+            return function( d , context ){
+                data = d;
+                return render.call(compenent,d, _escape );
+            };
+        else
+            return render.call(compenent,data, _escape );
+    };
+});
---
+++ release/pages/index/js/index.js
@@ -0,0 +1,33 @@
+define('index', function(require) {
+	var ls = require('$localStorage');
+	var template = require('$template');
+	require('comp-dataview');require('comp-dataview2');
+	require('comp-dataview3');require('comp-dataview4');
+	require('index.css');
+	return {
+		el: '#page_index',
+		pageView: "<div id=\"page_index\">\n\t<h1>INDEX PAGE</h1>\n\t<div class=\"dataview js-dataView\">\n\t</div>\n</div>",
+		init: function(params) {},
+		enter: function(params) {
+			console.log(params);
+			var data = {
+				"title": "包质量宠文，不看后悔",
+				"desc": "保质保量，一生一世一双人，宠文，爽文。保质保量，一生一世一双人，宠文，爽文。",
+				"items": [{
+					"title": "淡定为妃",
+					"author": "风过水无痕",
+				}, {
+					"title": "八宝妆",
+					"author": "月下蝶影",
+				}, {
+					"title": "那村那人那傻瓜",
+					"author": "福宝",
+				}],
+				"flag": "new"
+			};
+			var dataStr = template("<div>\n\t<%= this.require('comp-dataview') %>\n\t<%= this.require('comp-dataview2') %>\n\t<%= this.require('comp-dataview3') %>\n\t<%= this.require('comp-dataview4') %>\n</div>",data); // data2 this
+			this.el[0].querySelector('.js-dataView').innerHTML = dataStr;
+		},
+		leave: function() {}
+	}
+});
