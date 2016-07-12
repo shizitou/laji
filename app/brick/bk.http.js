c7c6940f47b724440c778f23d544dbfe1162cdf2
shizitou <you@example.com>
2016-07-12 22:25:16 +0800

    update to 3.5.1

--- app/brick/bk.http.js
+++ app/brick/bk.http.js
@@ -6,7 +6,22 @@ define('$http',['$config'], function(require) {
 	//屏蔽掉$上的ajaxAPI,让开发者强制使用$http模块
 	if(xhr)
 		delete $.ajax,delete $.get,delete $.post;
-		
+
+	function parseArguments(url, data, success, dataType) {
+		if(typeof url === 'object') // {url:, data:, success:}
+			return url;
+		if (typeof data === 'function') // 'url', func, 'json'
+			dataType = success, success = data, data = undefined;
+		if (typeof success !== 'function') // 'url', {}, 'json' 
+			dataType = success, success = undefined;
+		return {
+			url: url,
+			data: data,
+			success: success,
+			dataType: dataType
+		}
+	}
+
 	function ajax(options){
 		//这里可以做些处理
 		options.timeout || (options.timeout = config.ajaxTimeout );
@@ -18,15 +33,17 @@ define('$http',['$config'], function(require) {
 	var http = {
 		ajax: function(options){
 			if(!support)
-				throw new Error('your code must include jquery or zepto:ajax');
+				throw new Error('your code must include $.ajax');
 			return ajax(options);
 		},
-		get: function(options){
-			options.type = 'get'
+		get: function(/* url, data, success, dataType */){
+			var options = parseArguments.apply(null, arguments);
+			options.type = 'GET';
 			return http.ajax(options);
 		},
-		post: function(options){
-			options.type = 'post'
+		post: function(/* url, data, success, dataType */){
+			var options = parseArguments.apply(null, arguments);
+    		options.type = 'POST';
 			return http.ajax(options);
 		}
 	};
